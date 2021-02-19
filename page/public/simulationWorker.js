32cbf1a59cbae4205ece67c5cf1492afce4b5b5c
Mateusz Goik <59363479+Gojodzojo@users.noreply.github.com>
2021-02-19 09:52:03 +0100

    Simulator remake (#3)
    * Prepare to make simulator more similar to station mode
    
    * Implement basic satellite code in simulator
    
    * Add sending data frames to satellite simulation
    
    * Move satellite simulation to satellite source code

--- page/public/simulationWorker.js
+++ page/public/simulationWorker.js
@@ -30,8 +30,7 @@ function getCheckSum(byteArray) {
     return checkSum
 }
 
-let interval
-let simMetaData, leftEngineState, rightEngineState, heading, position, acceleration, velocity, startTime, simLoopNumber, messageBytes = []
+let simMetaData, leftEngineState, rightEngineState, heading, position, acceleration, velocity, time, simLoopNumber, messageBytes = [], windAcceleration, interval
 
 const airDensity = 1.225
 const gravityForceVal = 9.80665
@@ -40,29 +39,27 @@ onmessage = function({ data }) {
 
     if(data.action === "start") {      // Set simulation parameters and start simulation loop
         simMetaData = data.simMetaData
-        const { windAzimuth, canSatSurfaceArea, windSpeed, canSatMass, initialHeight } = simMetaData        
+        const { windAzimuth, canSatSurfaceArea, windSpeed, canSatMass, initialHeight } = simMetaData
+                
+        leftEngineState = true
+        rightEngineState = false
+        heading = 0
+        velocity = new Vector(0, 0, 0)
+        position = new Vector(0, initialHeight, 0)        
+        acceleration = new Vector(0, gravityForceVal * -1, 0)
         
-        startTime = Date.now()
-
-        const rad = windAzimuth * (Math.PI / 180)
-        const windAcceleration = new Vector( Math.cos(rad) * -1, 0, Math.sin(rad) * -1 )        
-        if (windAzimuth <= 180) {
-            windAcceleration.multiplyByScalar(-1)
-        }
+        const rad = (windAzimuth + 180) / 180 * Math.PI
+        windAcceleration = new Vector( Math.cos(rad), 0, Math.sin(rad) )
+        console.log(windAcceleration)
         
         /* https://www.engineeringtoolbox.com/wind-load-d_1775.html */
-        const windForceVal = 0.5 * canSatSurfaceArea * airDensity * Math.pow(windSpeed, 2);
+        const windForceVal = 0.5 * canSatSurfaceArea * airDensity * Math.pow(windSpeed, 2)
         windAcceleration.multiplyByScalar(windForceVal);
         windAcceleration.multiplyByScalar(1 / canSatMass);
         
-        acceleration = windAcceleration;
-        acceleration._y = gravityForceVal * -1;        
-        
-        velocity = new Vector(0, 0, 0)
-        position = new Vector(0, initialHeight, 0)
+        time = 0
         simLoopNumber = simMetaData.satelliteSimulationInterval / simMetaData.environmentSimulationInterval
-        
-        interval = setInterval(environmentSimulation, simMetaData.environmentSimulationInterval)        
+        interval = setInterval(environmentSimulation, simMetaData.environmentSimulationInterval)
     }
     // Stop simulation loop
     else if(data.action === "stop") {
@@ -82,11 +79,32 @@ onmessage = function({ data }) {
 function environmentSimulation() {
     if (simMetaData.satelliteSimulationInterval / simMetaData.environmentSimulationInterval <= simLoopNumber) {
         Module._loop();
+        time += simMetaData.satelliteSimulationInterval
         simLoopNumber = 0
     }
     simLoopNumber++
 
+    if(leftEngineState) {        
+        heading -= simMetaData.angularVelocity * (simMetaData.environmentSimulationInterval / 1000)
+    }
+    if(rightEngineState) {
+        heading += simMetaData.angularVelocity * (simMetaData.environmentSimulationInterval / 1000)
+    }
+
+    if(heading >= 360) {
+        heading -= 360
+    }
+    else if(heading < 0) {
+        heading += 360
+    }
+
+    const rad = (heading + 180) * (Math.PI / 180)
+    const forwardAcceleration = new Vector( Math.cos(rad), 0, Math.sin(rad))
+    forwardAcceleration.multiplyByScalar(simMetaData.forwardAccelerationVal);
+    
     velocity.addVector(acceleration, simMetaData.environmentSimulationInterval)
+    velocity.addVector(windAcceleration, simMetaData.environmentSimulationInterval)
+    velocity.addVector(forwardAcceleration, simMetaData.environmentSimulationInterval)
     position.addVector(velocity, simMetaData.environmentSimulationInterval)    
 
     // https://cnx.org/contents/TqqPA4io@2.43:olSre6jy@4/6-4-Si%C5%82a-oporu-i-pr%C4%99dko%C5%9B%C4%87-graniczna
@@ -98,7 +116,7 @@ const earthRadius = 6371e3
 const earthCircumference = Math.PI * 2 * earthRadius
 const degPerMeter = 360 / earthCircumference
 
-function getPressure(i) {
+function getPressure() {
     return 101325 * Math.pow(1-2.25577 * Math.pow(10, -5) * position._y, 5.25588) / 100
 }
 
@@ -107,18 +125,54 @@ function getTemperature() {
 }
 
 function getLatitude() {
-    return simMetaData.initialLongitude + position._x * degPerMeter        
+    const bearing = Math.atan2(position._z, position._x) - Math.PI / 2
+    const distance = Math.sqrt(simMetaData.initialLatitude * simMetaData.initialLatitude + simMetaData.initialLongitude * simMetaData.initialLongitude)
+    return pointRadialDistance(simMetaData.initialLatitude, simMetaData.initialLongitude, bearing, distance).lat
 }
 
 function getLongitude() {        
-    return simMetaData.initialLatitude + position._z * degPerMeter
+    const bearing = Math.atan2(position._z, position._x) - Math.PI / 2
+    const distance = Math.sqrt(simMetaData.initialLatitude * simMetaData.initialLatitude + simMetaData.initialLongitude * simMetaData.initialLongitude)
+    return pointRadialDistance(simMetaData.initialLatitude, simMetaData.initialLongitude, bearing, distance).lon
+    
 }
 
 function millis() {
-    return Date.now() - startTime
+    return time
 }
 
 function radio_available() {
     return messageBytes.length   
 }
 
+const rEarth = 6371.01 
+const epsilon = 0.000001
+
+function deg2rad(angle) {
+    return angle*Math.PI/180
+}
+
+function rad2deg(angle) {
+    return angle*180/Math.PI
+}
+
+
+function pointRadialDistance(lat1, lon1, bearing, distance) {
+    const rlat1 = deg2rad(lat1)
+    const rlon1 = deg2rad(lon1)
+    const rbearing = deg2rad(bearing)
+    const rdistance = distance / rEarth
+
+    const rlat = Math.asin( Math.sin(rlat1) * Math.cos(rdistance) + Math.cos(rlat1) * Math.sin(rdistance) * Math.cos(rbearing) )
+
+    let rlon
+    if (Math.cos(rlat) === 0 || Math.abs(Math.cos(rlat)) < epsilon) {
+        rlon=rlon1
+    }
+    else {
+        rlon = ( (rlon1 - Math.asin( Math.sin(rbearing)* Math.sin(rdistance) / Math.cos(rlat) ) + Math.PI ) % (2*Math.PI) ) - Math.PI
+    }
+    const lat = rad2deg(rlat)
+    const lon = rad2deg(rlon)
+    return {lat, lon}
+}
